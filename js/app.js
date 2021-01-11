948cb95996b1b6635528e9a03fbb41ad630f7c01
shakhi95 <shakhiii2019@gmail.com>
2021-01-11 12:16:59 +0330

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+
+function fadeOut(el){
+    el.style.opacity = 1;
+  
+    (function fade() {
+      if ((el.style.opacity -= .1) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
+  
+function fadeIn(el){
+    el.style.opacity = 0;
+    el.style.display ="block";
+  
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += .1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
+
+document.getElementById("content").style.display = "none";
+
+setTimeout(()=>{
+
+fadeOut(document.getElementById("loader"));
+fadeIn(document.getElementById("content"))
+},1500);
